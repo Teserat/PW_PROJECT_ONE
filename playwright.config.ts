b1b76fae8891ce55ba6fe12178745ddb309d20a8
Lu.Teserat <dobry.tester.pl@gmail.com>
2025-01-13 00:01:51 +0100

    added small fix for allure raport

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['allure-playwright', {outputFolder: 'test-results'}], ['line']],
+    ['allure-playwright', {outputFolder: 'allure-results'}], ['list']],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
