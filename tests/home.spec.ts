0072a36b5eed44d3d2b31980f1b5db0e6a84e241
Lu.Teserat <dobry.tester.pl@gmail.com>
2025-01-12 20:54:54 +0100

    add extra test + config allure

---
+++ tests/home.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+import { WelcomePage } from './pages/WelcomePage';
+
+//basic concept test
+test.describe('Home', () => {
+    test("Open HomePage", async ({page}) => {
+        const welcomePage = new WelcomePage(page);
+        await welcomePage.goto();
+        await expect(page).toHaveTitle('Zabawna Strona Internetowa')
+    });
+})
